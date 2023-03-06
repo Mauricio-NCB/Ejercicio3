a2f88e13f7966efaa9fa9aae6d579fa701e57a0f
Danielda <61012696+BoolDan@users.noreply.github.com>
2023-03-06 12:11:19 +0100

    Aplicacion huevona

---
+++ 03-inicio/includes/Aplication.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class Aplicacion {  
+    private static $instancia;
+
+    
+
+    public static Aplicacion getInstance(){
+        if (app!=null){
+            return self::$instancia;
+        }
+    }
+
+
+
+}
