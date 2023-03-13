7337d882aae7edf3d4a3ff790f6805197ba2668b
Danielda <61012696+BoolDan@users.noreply.github.com>
2023-03-13 13:17:07 +0100

    Cambios Aplicación y Usuario

--- 03-inicio/includes/Aplication.php
+++ 03-inicio/includes/Aplication.php
@@ -3,15 +3,57 @@
 
 class Aplicacion {  
     private static $instancia;
+    private $iniciada = false;
+    private $conn;
+    private $dbDatosdeConexion;
 
+    function __construct(){
+
+    }
     
+    public static function getInstance(){
+        if (self::$instancia != null){
+            self::$instancia = new static();
+        }
+        return self::$instancia;
+    }
 
-    public static Aplicacion getInstance(){
-        if (app!=null){
-            return self::$instancia;
+    function init($dbDatosdeConexion){
+        if (!$this->iniciada){
+            $this->dbDatosdeConexion = $dbDatosdeConexion;
+            $this->iniciada = true;
         }
     }
 
+    public function shutdown(){
+        $this->compruebaInstanciaIniciada();
+    }
+
+    private function compruebaInstanciaIniciada(){
+        if (!$this->iniciada){
+            echo "Aplicación no iniciada";
+            exit();
+        }
+    }
+
+    function getConexiónBd(){
+        if (!$this->conn){
+            $host = $this->dbDatosdeConexion['host'];
+            $user = $this->dbDatosdeConexion['user'];
+            $pass = $this->dbDatosdeConexion['pass'];
+            $db = $this->dbDatosdeConexion['db'];
 
 
+            $conn = new mysqli($host, $user, $pass, $db);
+            if ($conn->connect_error){
+                die("La conexión ha fallado" . $conn->connect_error);
+            }
+            if (!$conn->set_charset('utfmb8')){
+                echo "Error al configurar la BD ({$conn->errno}): {$conn->error}";
+                exit();
+            }
+            $this->conn = $conn;
+        }
+        return $this->conn;
+    }
 }
