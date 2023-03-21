8b73abe6586f202eb8e3e11ef4f7b9ebcc8cc985
Mauricio-NCB <mncalderon2001@gmail.com>
2023-03-21 20:27:46 +0100

    pequeñas correcciones

--- 01-inicio/procesarLogin.php
+++ 01-inicio/procesarLogin.php
@@ -39,48 +39,33 @@ if (count($erroresFormulario) === 0) {
 		$erroresFormulario[] = "El usuario o el password no coinciden";
 	}
 }
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title>Login</title>
-	<link rel="stylesheet" type="text/css" href="css/estilo.css" />
-</head>
-<body>
-<div id="contenedor">
-<?php
-require('includes/vistas/comun/cabecera.php');
-require('includes/vistas/comun/sidebarIzq.php');
-?>
-<main>
-	<article>
-		<h1>Acceso al sistema</h1>
-		<?= generaErroresGlobalesFormulario($erroresFormulario) ?>
-		<form action="procesarLogin.php" method="POST">
-		<fieldset>
-            <legend>Usuario y contraseña</legend>
-            <div>
-                <label for="nombreUsuario">Nombre de usuario:</label>
-				<input id="nombreUsuario" type="text" name="nombreUsuario" value="<?= $nombreUsuario ?>" />
-				<?=  generarError('nombreUsuario', $erroresFormulario) ?>
-            </div>
-            <div>
-                <label for="password">Password:</label>
-				<input id="password" type="password" name="password" value="<?= $password ?>" />
-				<?=  generarError('password', $erroresFormulario) ?>
-            </div>
-            <div>
-				<button type="submit" name="login">Entrar</button>
-			</div>
-		</fieldset>
-		</form>
-	</article>
-</main>
-<?php
-require('includes/vistas/comun/sidebarDer.php');
-require('includes/vistas/comun/pie.php');
-?>
-</div>
-</body>
-</html>
+
+$tituloPagina = 'Login';
+
+$erroresGlobalesFormulario = generaErroresGlobalesFormulario($erroresFormulario);
+$erroresCampos = generaErroresCampos(['nombreUsuario', 'password'], $erroresFormulario);
+
+$contenidoPrincipal= <<<EOS
+<h1>Acceso al sistema</h1>
+$erroresGlobalesFormulario
+<form action="procesarLogin.php" method="POST">
+<fieldset>
+	<legend>Usuario y contraseña</legend>
+	<div>
+		<label for="nombreUsuario">Nombre de usuario:</label>
+		<input id="nombreUsuario" type="text" name="nombreUsuario" value="$nombreUsuario" />
+		{$erroresCampos['nombreUsuario']}
+	</div>
+	<div>
+		<label for="password">Password:</label>
+		<input id="password" type="password" name="password" value="$password" />
+		{$erroresCampos['password']}
+	</div>
+	<div>
+		<button type="submit" name="login">Entrar</button>
+	</div>
+</fieldset>
+</form>
+EOS;
+
+require('includes/vistas/plantillas/plantilla.php');
