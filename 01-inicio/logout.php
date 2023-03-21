d69cc7e862c290db543d260a9d5fc77fde2e5eb9
Mauricio-NCB <mncalderon2001@gmail.com>
2023-03-21 23:03:28 +0100

    Update logout.php

--- 01-inicio/logout.php
+++ 01-inicio/logout.php
@@ -8,29 +8,11 @@ unset($_SESSION['nombre']);
 
 
 session_destroy();
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title>Logout</title>
-	<link rel="stylesheet" type="text/css" href="css/estilo.css" />
-</head>
-<body>
-<div id="contenedor">
-<?php
-require('includes/vistas/comun/cabecera.php');
-require('includes/vistas/comun/sidebarIzq.php');
-?>
-<main>
-	<article>
-		<h1>Hasta pronto!</h1>
-	</article>
-</main>
-<?php
-require('includes/vistas/comun/sidebarDer.php');
-require('includes/vistas/comun/pie.php');
-?>
-</div>
-</body>
-</html>
+
+$tituloPagina = 'Logout';
+
+$contenidoPrincipal = <<<EOS
+<h1>Hasta pronto!</h1>
+EOS;
+
+require 'includes/vistas/plantillas/plantilla.php';
